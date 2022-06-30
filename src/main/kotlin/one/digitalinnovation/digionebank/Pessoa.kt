171bf44da6ace0ea6a4c172609b8e7932bc72644
thaisperlho <carvalho.thais0301@gmail.com>
2022-06-30 12:55:51 -0300

    Criando a primeira class em Kotlin✅

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String  = "Thais"
+    var cpf: String = "123.123.123-10"
+}
+
+fun main() {
+     val thais = Pessoa()
+
+    println(thais.nome)
+    println(thais.cpf)
 }
