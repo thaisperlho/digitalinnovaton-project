9cb52f4fccf8ed37a37504f58d4fc5e24003b90e
thaisperlho <carvalho.thais0301@gmail.com>
2022-06-30 15:05:59 -0300

    updated to make person en employee abstract class 🔄

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.digionebank.testes
-
-import one.digitalinnovation.digionebank.Pessoa
-
-fun main() {
-    val thais = Pessoa(nome = "Thais", cpf = "123.345.678-90")
-    println(thais.nome)
-    println(thais.cpf)
-}
