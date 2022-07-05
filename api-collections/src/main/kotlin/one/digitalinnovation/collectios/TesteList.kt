be73eb9ae639b0eb94742ceab03031899887a85b
thaisperlho <carvalho.thais0301@gmail.com>
2022-07-05 15:13:33 -0300

    upedated list tests using sortedBy ✅

--- api-collections/src/main/kotlin/one/digitalinnovation/collectios/TesteList.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collectios/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collectios
 
 fun main() {
-    val carlos = Funcionario(nome = "Carlos", salario = 1000.00)
-    val ana = Funcionario(nome = "Ana", salario = 2000.00)
-    val thais = Funcionario(nome = "Thais", salario = 4000.00)
+    val carlos = Funcionario(nome = "Carlos", salario = 3000.00)
+    val ana = Funcionario(nome = "Ana", salario = 5000.00)
+    val thais = Funcionario(nome = "Thais", salario = 1500.00)
 
     val funcionarios = listOf(carlos, ana, thais)
 
@@ -12,6 +12,10 @@ fun main() {
     println("---------")
     println(funcionarios.find { it.nome == "Thais" })
 
+    println("---------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
 }
 
 data class Funcionario(
