e12ec743bf07b2ed70793d121216779372a606af
thaisperlho <carvalho.thais0301@gmail.com>
2022-07-05 14:57:11 -0300

    upadeted collection operations ✅

--- api-collections/src/main/kotlin/one/digitalinnovation/collectios/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collectios/TesteOperacoes.kt
@@ -8,8 +8,8 @@ fun main() {
     }
 
     println("-------------")
-    println("Maior salário: ${salarios.max()}")
-    println("Menor salário: ${salarios.min()}")
+    println("Maior salário: ${salarios.maxOrNull()}")
+    println("Menor salário: ${salarios.minOrNull()}")
     println("Média salarial: ${salarios.average()}")
 
     val salariosMaiorQue2000 = salarios.filter { it > 2000.0 }
