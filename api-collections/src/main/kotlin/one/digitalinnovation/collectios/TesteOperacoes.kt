a92035a7f8ee5a479c303953d10612e0999b1c57
thaisperlho <carvalho.thais0301@gmail.com>
2022-07-05 14:55:07 -0300

    upadeted collection operations ✅

--- api-collections/src/main/kotlin/one/digitalinnovation/collectios/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collectios/TesteOperacoes.kt
@@ -15,4 +15,17 @@ fun main() {
     val salariosMaiorQue2000 = salarios.filter { it > 2000.0 }
     println("-------------")
     salariosMaiorQue2000.forEach { println(it) }
+
+
+    println("-------------")
+    println(salarios.count { it in 2000.0..5500.0 })
+
+    println("-------------")
+    println(salarios.find { it == 2250.0})
+    println(salarios.find { it == 250.0})
+
+    println("-------------")
+    println(salarios.any { it == 1200.0})
+    println(salarios.any { it == 200.0})
+
 }
